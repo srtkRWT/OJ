3eb23996e17ae10df9b92a099219aa4f1deb1fc6
srtkrwt <sarthak0312@gmail.com>
2020-02-19 09:42:18 +0530

    changed index

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const {
+    DB_CONNECT,
+    PORT = 3000
+} = process.env;
+
+mongoose.connect(DB_CONNECT,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    () => console.log('connected to db')
+);
+
+app.use(express.json());
+
+const adminRoute = require('.admin/route');
+const mcqRoute = require('.mcq/route');
+const moderatorRoute = require('.moderator/route');
+const studentRoute = require('.student/route');
+const testRoute = require('.test/route');
+const test_seriesRoute = require('.test_series/route');
+
+
+app.use('/api/admin', adminRoute);
+app.use('/api/mcq', mcqRoute);
+app.use('/api/moderator', moderatorRoute);
+app.use('/api/student', studentRoute);
+app.use('/api/test', testRoute);
+app.use('/api/test_series', test_seriesRoute);
+
+app.listen(PORT, () => {
+    console.log("running server");
+});
