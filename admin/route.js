c4cffae910900442fe95f9c970293e8a8c1ac773
srtkrwt <sarthak0312@gmail.com>
2020-03-02 22:29:46 +0530

    ...

--- admin/route.js
+++ admin/route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Admin = require('./model/Admin');
 const Joi = require('@hapi/joi');
 
-const schema = {
+const schema = Joi.object({
     name: Joi.string()
         .min(6)
         .required(),
@@ -13,10 +13,40 @@ const schema = {
     password: Joi.string()
         .min(6)
         .required()
-};
+});
 
+//get all admins
 router.get('/', async (req, res) => {
-    res.send("Admin");
+    const id = req.params.id;
+    const result = await Admin.find();
+
+    console.log(result);
+    res.send(result);
+});
+
+
+// add new admin
+router.post('/', async (req, res) => {
+    console.log(req.body);
+
+    const validation = schema.validate(req.body);
+
+    if (validation.error) {
+        res.status(400).send(validation.error);
+        return;
+    }
+    const admin = new Admin({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const saveAdmin = await admin.save();
+        res.send(saveAdmin);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 
